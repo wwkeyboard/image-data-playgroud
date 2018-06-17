d5951cdf7f86acf87391e88582c54adf31b83342
Aaron Lee <alee@aaronosaur.us>
2018-06-16 19:35:31 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::BufReader;
+extern crate exif;
+
+fn main() {
+    println!("Hello, world!");
+
+    let filename = "test.ARW";
+    let file = File::open(filename).expect("opening file");
+
+    let reader = exif::Reader::new(&mut BufReader::new(&file)).expect("building reader");
+
+    for f in reader.fields() {
+        let thumb = if f.thumbnail {"1/"} else {"0/"};
+        println!(" {}{}: {}", thumb, f.tag, f.value.display_as(f.tag));
+    }
+
+
+}

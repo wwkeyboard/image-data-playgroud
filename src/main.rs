638c4c1707fbd0fd5fe1469e0befa7e9c697d28c
Aaron Lee <alee@aaronosaur.us>
2018-06-23 10:05:49 -0500

    extract raw reading

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,30 @@ use std::io::BufReader;
 use std::env;
 extern crate exif;
 
-fn main() {
-    let filename: String = env::args().nth(1).expect("must supply filename");
 
-    println!("reading {}", filename);
-    let file = File::open(filename).expect("opening file");
+fn read_raw(filename: &str) -> Result<String, String> {
+    let file = File::open(&filename).expect("opening file");
 
-    let reader = exif::Reader::new(&mut BufReader::new(&file)).expect("building reader");
+    let reader = match exif::Reader::new(&mut BufReader::new(&file)){
+        Ok(r) => r,
+        Err(e) => return Err("couldn't read file".to_string()),
+    };
+
+    let mut accum = String::new();
 
     for f in reader.fields() {
             let thumb = if f.thumbnail {"1/"} else {"0/"};
-            println!(" {} --{}--: {}", thumb, f.tag, f.value.display_as(f.tag));
+            accum += &format!(" {} --{}--: {}\n", thumb, f.tag, f.value.display_as(f.tag));
         }
+
+    Ok(accum)
+}
+
+fn main() {
+    let filename: String = env::args().nth(1).expect("must supply filename");
+
+    let dat = read_raw(&filename).expect("reading file");
+
+    println!("{}", dat)
+
 }
